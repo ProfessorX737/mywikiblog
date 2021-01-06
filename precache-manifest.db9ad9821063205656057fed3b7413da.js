8a4232696c4859b2753525514e87a4228ea336b0
Xavier Poon <xavierpoon737@gmail.com>
2021-01-06 20:45:05 +1100

    Updates

---
+++ precache-manifest.db9ad9821063205656057fed3b7413da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d408f8036aaaf117d976b97e7b8fea",
+    "url": "/mywikiblog/index.html"
+  },
+  {
+    "revision": "5be715dd9faea19c0dba",
+    "url": "/mywikiblog/static/css/2.56a1fa8e.chunk.css"
+  },
+  {
+    "revision": "ca3c937071347b5da734",
+    "url": "/mywikiblog/static/css/main.f503e2f1.chunk.css"
+  },
+  {
+    "revision": "5be715dd9faea19c0dba",
+    "url": "/mywikiblog/static/js/2.e804a4e8.chunk.js"
+  },
+  {
+    "revision": "c21444f4c179eb12b9bedc3b20f5d17f",
+    "url": "/mywikiblog/static/js/2.e804a4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3c937071347b5da734",
+    "url": "/mywikiblog/static/js/main.34e0c1dd.chunk.js"
+  },
+  {
+    "revision": "5916ec27974c96e87dd0",
+    "url": "/mywikiblog/static/js/runtime-main.1a2791a6.js"
+  }
+]);
